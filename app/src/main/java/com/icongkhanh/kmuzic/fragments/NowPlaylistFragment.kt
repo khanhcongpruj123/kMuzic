55a7e0d05b31450fb5c638be9fc0d678ef83b88a
Tran Duy Cong Khanh <khanhcongpruj123@gmail.com>
2020-03-01 11:14:02 +0700

    UI for Now Playing Fragment

--- app/src/main/java/com/icongkhanh/kmuzic/fragments/NowPlaylistFragment.kt
+++ app/src/main/java/com/icongkhanh/kmuzic/fragments/NowPlaylistFragment.kt
@@ -5,18 +5,16 @@ import android.os.Bundle
 import android.util.Log
 import android.view.*
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatSeekBar
-import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.icongkhanh.kmuzic.R
-import com.icongkhanh.kmuzic.adapters.ListMusicAdapter
 import com.icongkhanh.kmuzic.playermuzicservice.MuzicState
+import com.icongkhanh.kmuzic.utils.BitmapUtils
 import com.icongkhanh.kmuzic.viewmodels.NowPlaylistViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -28,14 +26,16 @@ class NowPlaylistFragment : Fragment() {
 
     private val viewmodel: NowPlaylistViewModel by viewModel()
 
-    lateinit var listMuzic: RecyclerView
-    lateinit var adapterMuzic: ListMusicAdapter
+    //    lateinit var listMuzic: RecyclerView
+//    lateinit var adapterMuzic: ListMusicAdapter
+    lateinit var imgThumbnail: ImageView
     lateinit var btnPlayOrPause: ImageButton
     lateinit var btnNext: ImageButton
     lateinit var btnPrevious: ImageButton
     lateinit var seekBar: AppCompatSeekBar
     lateinit var tvMusicName: TextView
     lateinit var tvAuthorName: TextView
+    lateinit var btnBack: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,13 +51,15 @@ class NowPlaylistFragment : Fragment() {
 
         val view = inflater.inflate(R.layout.fragment_now_playlist, container, false)
 
-        listMuzic = view.findViewById(R.id.list_music)
+//        listMuzic = view.findViewById(R.id.list_music)
         btnNext = view.findViewById(R.id.btn_next)
         btnPlayOrPause = view.findViewById(R.id.btn_play_or_pause)
         btnPrevious = view.findViewById(R.id.btn_previous)
         seekBar = view.findViewById(R.id.music_progress)
         tvAuthorName = view.findViewById(R.id.author_name)
         tvMusicName = view.findViewById(R.id.music_name)
+        imgThumbnail = view.findViewById(R.id.thumbnail)
+        btnBack = view.findViewById(R.id.btn_back)
 
         btnPlayOrPause.setOnClickListener {
             viewmodel.onPressPlayOrPause()
@@ -71,17 +73,21 @@ class NowPlaylistFragment : Fragment() {
             viewmodel.onPressPrevious()
         }
 
-        adapterMuzic = ListMusicAdapter(context!!)
-
-        adapterMuzic.setOnPressItem {
-            viewmodel.onPressItemMuzic(it)
+        btnBack.setOnClickListener {
+            findNavController().navigateUp()
         }
 
-        listMuzic.adapter = adapterMuzic
-
-        viewmodel.listMusic.observe(viewLifecycleOwner, Observer {
-            adapterMuzic.updateListMuisc(it)
-        })
+//        adapterMuzic = ListMusicAdapter(context!!)
+//
+//        adapterMuzic.setOnPressItem {
+//            viewmodel.onPressItemMuzic(it)
+//        }
+//
+//        listMuzic.adapter = adapterMuzic
+//
+//        viewmodel.listMusic.observe(viewLifecycleOwner, Observer {
+//            adapterMuzic.updateListMuisc(it)
+//        })
 
         // subcribe Ui
         viewmodel.stateMuzic.observe(viewLifecycleOwner, Observer {
@@ -99,11 +105,13 @@ class NowPlaylistFragment : Fragment() {
             seekBar.progress = (it * 100).toInt()
         })
 
-        viewmodel.currentPlayingMuzic.observe(viewLifecycleOwner, Observer {currMusic ->
+        viewmodel.currentPlayingMuzic.observe(viewLifecycleOwner, Observer { currMusic ->
             Log.d(TAG, "On Oserver Current Muzic: ${currMusic.name}")
             currMusic?.let { muzic ->
                 tvAuthorName.text = muzic.authorName
                 tvMusicName.text = muzic.name
+                Glide.with(this).load(BitmapUtils.getBitmapFromMusicFile(muzic.path))
+                    .into(imgThumbnail)
             }
         })
 
